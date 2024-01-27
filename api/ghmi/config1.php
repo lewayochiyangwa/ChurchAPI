793a41eab3d33fc0ed94b798707ab63df11a9ad9
lewayochiyangwa <lewayo.chiyangwa@gmail.com>
2024-01-27 22:38:26 +0200

    h

--- api/ghmi/config1.php
+++ api/ghmi/config1.php
@@ -2,11 +2,11 @@
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'leroy');
-define('DB_PASSWORD', 'leroy');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'L3r0y');
 define('DB_NAME', 'church');
  
-/* Attempt to connect to MySQL database */
+/* Attempt to connect to MySQL database */ //;ast trial
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
---
+++ api/ghmi/testx.php
@@ -0,0 +1,16 @@
+<?php
+/////////////////////////////////////////////////////////
+/include('config1.php');
+
+
+
+if($link)
+{
+    echo "Connection established.\n";
+}
+else
+{
+    //echo "Connection could not be establisheds.\n";
+    die("Failed");
+}
+
